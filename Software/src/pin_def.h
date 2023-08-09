f70480df7d370ed65bac3f394fb9c0695b2d11d3
Leif <cerberus81@gmail.com>
2023-08-09 16:12:33 +0200

    added pin definitions for pcb

--- Software/src/pin_def.h
+++ Software/src/pin_def.h
@@ -1,1 +1,30 @@
 #pragma once
+
+// U2 Connector
+#define ENC1_SW = 4 
+#define ENC1_A = 5
+#define ENC1_B = 6
+// U3 Connector
+#define ENC2_SW = 7
+#define ENC2_A = 15
+#define ENC2_B = 16
+// U4 Connector
+#define ENC3_SW = 17
+#define ENC3_A = 18
+#define ENC3_B = 8
+// U5 Connector
+#define ENC4_SW = 28
+#define ENC4_A = 29
+#define ENC4_B = 30
+// U6 Connector
+#define ENC5_SW = 9
+#define ENC5_A = 10
+#define ENC5_B = 11
+// Pedal 1 Connector
+#define PED1 = 12
+// Pedal 2 Connector
+#define PED2 = 13
+
+// I2C
+#define I2C_SDA = 1
+#define I2C_SCL = 2
