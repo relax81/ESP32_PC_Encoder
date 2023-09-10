59b361d61b8495da5ac016cf3a59aa7a977e1744
Leif <cerberus81@gmail.com>
2023-09-10 04:17:02 +0200

    added enc 3 4 and 5. need function to clean up

--- Software/src/main.cpp
+++ Software/src/main.cpp
@@ -12,8 +12,11 @@ unsigned long longPressAfterMiliseconds = 1000;  //how long čong press shoud be
 // Encoder Test
 #define ENCODER_STEPS_PER_NOTCH    4   // Change this depending on which encoder is used
 
-ClickEncoder encoder = ClickEncoder(ENC1_A,ENC1_B,ENC1_BTN,ENCODER_STEPS_PER_NOTCH);
+ClickEncoder encoder1 = ClickEncoder(ENC1_A,ENC1_B,ENC1_BTN,ENCODER_STEPS_PER_NOTCH);
 ClickEncoder encoder2 = ClickEncoder(ENC2_A,ENC2_B,ENC2_BTN,ENCODER_STEPS_PER_NOTCH);
+ClickEncoder encoder3 = ClickEncoder(ENC3_A,ENC3_B,ENC3_BTN,ENCODER_STEPS_PER_NOTCH);
+ClickEncoder encoder4 = ClickEncoder(ENC4_A,ENC4_B,ENC4_BTN,ENCODER_STEPS_PER_NOTCH);
+ClickEncoder encoder5 = ClickEncoder(ENC5_A,ENC5_B,ENC5_BTN,ENCODER_STEPS_PER_NOTCH);
 
 BleKeyboard bleKeyboard;
 
@@ -23,10 +26,17 @@ void setup() {
   Serial.println("Starting BLE work!");
 
   // Encoder test
-  encoder.setButtonHeldEnabled(true);
-  encoder.setDoubleClickEnabled(true);
+  encoder1.setButtonHeldEnabled(true);
+  encoder1.setDoubleClickEnabled(true);
   encoder2.setButtonHeldEnabled(true);
   encoder2.setDoubleClickEnabled(true);
+  encoder3.setButtonHeldEnabled(true);
+  encoder3.setDoubleClickEnabled(true);
+  encoder4.setButtonHeldEnabled(true);
+  encoder4.setDoubleClickEnabled(true);
+  encoder5.setButtonHeldEnabled(true);
+  encoder5.setDoubleClickEnabled(true);
+
 
   bleKeyboard.begin();
 }
@@ -71,13 +81,16 @@ void loop() {
   static uint32_t lastService = 0;
   if (micros() - lastService >= 1000) {
     lastService = micros();                
-    encoder.service();  
+    encoder1.service();  
     encoder2.service();
+    encoder3.service();
+    encoder4.service();
+    encoder5.service();
   }
 
  
 static int16_t last, value;
-    value += encoder.getValue();
+    value += encoder1.getValue();
   
   if (value != last) {
     last = value;
@@ -95,12 +108,42 @@ static int16_t last, value;
     Serial.println(value2);
 
   }
+
+  static int16_t last3, value3;
+    value3 += encoder3.getValue();
   
-  ClickEncoder::Button b = encoder.getButton();
-  if (b != ClickEncoder::Open) {
-    Serial.print("Button: ");
+  if (value3 != last3) {
+    last3 = value3;
+    Serial.print("Encoder 3 Value: ");
+    Serial.println(value3);
+
+  }
+
+    static int16_t last4, value4;
+    value4 += encoder4.getValue();
+  
+  if (value4 != last4) {
+    last4 = value4;
+    Serial.print("Encoder 4 Value: ");
+    Serial.println(value4);
+
+  }
+
+    static int16_t last5, value5;
+    value5 += encoder5.getValue();
+  
+  if (value5 != last5) {
+    last5 = value5;
+    Serial.print("Encoder 5 Value: ");
+    Serial.println(value5);
+
+  }
+  
+  ClickEncoder::Button b1 = encoder1.getButton();
+  if (b1 != ClickEncoder::Open) {
+    Serial.print("Button 1: ");
     #define VERBOSECASE(label) case label: Serial.println(#label); break;
-    switch (b) {
+    switch (b1) {
       VERBOSECASE(ClickEncoder::Pressed);
       VERBOSECASE(ClickEncoder::Held)
       VERBOSECASE(ClickEncoder::Released)
@@ -111,7 +154,7 @@ static int16_t last, value;
 
   ClickEncoder::Button b2 = encoder2.getButton();
   if (b2 != ClickEncoder::Open) {
-    Serial.print("Button: ");
+    Serial.print("Button 2: ");
     #define VERBOSECASE(label) case label: Serial.println(#label); break;
     switch (b2) {
       VERBOSECASE(ClickEncoder::Pressed);
@@ -121,5 +164,45 @@ static int16_t last, value;
       VERBOSECASE(ClickEncoder::DoubleClicked)
     }
   }  
+
+  ClickEncoder::Button b3 = encoder3.getButton();
+  if (b3 != ClickEncoder::Open) {
+    Serial.print("Button 3: ");
+    #define VERBOSECASE(label) case label: Serial.println(#label); break;
+    switch (b3) {
+      VERBOSECASE(ClickEncoder::Pressed);
+      VERBOSECASE(ClickEncoder::Held)
+      VERBOSECASE(ClickEncoder::Released)
+      VERBOSECASE(ClickEncoder::Clicked)
+      VERBOSECASE(ClickEncoder::DoubleClicked)
+    }
+  }  
+
+  ClickEncoder::Button b4 = encoder4.getButton();
+  if (b4 != ClickEncoder::Open) {
+    Serial.print("Button 4: ");
+    #define VERBOSECASE(label) case label: Serial.println(#label); break;
+    switch (b4) {
+      VERBOSECASE(ClickEncoder::Pressed);
+      VERBOSECASE(ClickEncoder::Held)
+      VERBOSECASE(ClickEncoder::Released)
+      VERBOSECASE(ClickEncoder::Clicked)
+      VERBOSECASE(ClickEncoder::DoubleClicked)
+    }
+  }  
+
+  ClickEncoder::Button b5 = encoder5.getButton();
+  if (b5 != ClickEncoder::Open) {
+    Serial.print("Button 5: ");
+    #define VERBOSECASE(label) case label: Serial.println(#label); break;
+    switch (b5) {
+      VERBOSECASE(ClickEncoder::Pressed);
+      VERBOSECASE(ClickEncoder::Held)
+      VERBOSECASE(ClickEncoder::Released)
+      VERBOSECASE(ClickEncoder::Clicked)
+      VERBOSECASE(ClickEncoder::DoubleClicked)
+    }
+  }  
+
 }
 
